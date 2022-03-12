e7049bafd9504c9ebbc3abc568f07ff43d568fc8
Alatise Oluwaseun <holynation667@gmail.com>
2022-03-12 02:50:41 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - Entry point 
+ *main - Entry point
  *Description: Print 0-9 separated with commas
  *and a space, using putchar four times max only
  *Return: Always return 0
