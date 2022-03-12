fd03d67042985e7edc75c36704dece240421af7e
Alatise Oluwaseun <holynation667@gmail.com>
2022-03-12 14:14:26 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,7 +28,8 @@ int main(void)
 					putchar(t);
 					putchar(u);
 
-					if (!((tens == '9' && unit == '8') && (t == '9' && u == '9')))
+					if (!((tens == '9' && unit == '8') &&
+					      (t == '9' && u == '9')))
 					{
 						putchar(',');
 						putchar(' ');
