c7999fdde9197f55094db31f5a8bf32240da9654
holynation <holynation667@gmail.com>
2022-03-17 15:25:09 -0700

    adjusted the _isupper betty bug

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 /**
   * _isupper - checks for uppercase character
   * @c: integer to be checked
-  * @Return: 1 if uppercase,0 if otherwise
+  * Return: 1 if uppercase,0 if otherwise
   */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);		
+		return (1);
 	}
 	else
 	{
