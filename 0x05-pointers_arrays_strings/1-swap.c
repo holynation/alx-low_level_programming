6ce7b449697936d071f1dbf2c801cc110fa36b8f
Alatise Oluwaseun <holynation667@gmail.com>
2022-03-22 03:53:59 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * swat_int - swaps the values of two integers.
+  * swap_int - swaps the values of two integers.
   * @a: integer to swap
   * @b: integer to swap
   */
 
-int swap_int(int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int tmp; /* to temp hold the value of a single int*/
 
