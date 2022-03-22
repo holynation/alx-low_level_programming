24fb6f74b3eeaafb6e6856687ee7fa751ada831b
holynation <holynation667@gmail.com>
2022-03-21 20:47:55 -0700

    fixing whitespace

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -28,14 +28,16 @@ void rev_string(char *s)
 {
 	int length, c;
 	char *begin, *end, temp;
+
 	length = _strlen(s);
 	begin  = s;
 	end    = s;
 
-	for (c = 0; c < length - 1; c++)
+	for (c = 0; c < length - 1; c++){
 		end++;
+	}
 	
-	for (c = 0; c < length/2; c++)
+	for (c = 0; c < length / 2; c++)
 	{
 		temp = *end;
 		*end = *begin;
