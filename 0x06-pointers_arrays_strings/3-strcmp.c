87202ea5ceccdbbb662d441f77d14c340deef219
holynation <holynation667@gmail.com>
2022-03-24 15:39:40 -0700

    fx the bug on strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-        // excluding the null byte from the string
-        while (*s1 != '\0' || *s2 != '\0')
-        {
-		// if element of arr1 in arr2 are diff, return difference
-                if (*s1 != *s2)
+	while (*s1 != '\0' || *s2 != '\0')/* excluding the null byte from the string*/
+	{
+		if (*s1 != *s2)/* if element of arr1 in arr2 are diff, return difference */
 		{
-                        return (*s1 - *s2);
+			return (*s1 - *s2);
 		}
-		// increment index
-                s1++;
-                s2++;
-        }
-        return (0);
+		s1++;
+		s2++;
+	}
+	return (0);
 }
