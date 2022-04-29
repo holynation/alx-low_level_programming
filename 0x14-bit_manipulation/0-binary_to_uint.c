d3cb809860dd9d1152ef32e662e3eb2534bd2c3c
Alatise Oluwaseun <holynation667@gmail.com>
2022-04-29 02:34:12 +0100

    fixed 0-binary code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,22 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - converst binary to uint
+ * @b: binary number
+ * Return: converted uint
  */
 
 unsigned int binary_to_uint(const char *b)
 {
+	uint a = 0;
 
-	int len = 0, i;
-	unsigned int sum = 0;
-
-	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-	/* iterate string and if '1' then multiply by power of 2 */
-	i = 0;
-	while (b[i])
+	if (!b)
+		return (0);
+	while (*b)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (*b != '1' && *b != '0')
+			return (0);
+		a = a * 2 + (*b++ - 48);
 	}
-
-	return (sum);
+	return (a);
 }
